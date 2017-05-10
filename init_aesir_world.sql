5970acdd056fc3413e9b5261d9bfc8e535f0f889
Michael de Lang <kingoipo@gmail.com>
2017-05-10 20:10:28 +0200

    Initial, broken, commit

---
+++ init_aesir_world.sql
@@ -0,0 +1,136 @@
+CREATE TABLE scripts (
+    id SERIAL PRIMARY KEY,
+    "name" citext NOT NULL,
+    script text NOT NULL
+);
+
+CREATE TABLE script_triggers (
+    id SERIAL PRIMARY KEY,
+    trigger_type citext NOT NULL,
+    script_id INT NOT NULL,
+    global BOOLEAN NOT NULL,
+);
+
+CREATE TABLE script_zones (
+    id SERIAL PRIMARY KEY,
+    x BIGINT NOT NULL,
+    y BIGINT NOT NULL,
+    width BIGINT NOT NULL,
+    height BIGINT NOT NULL
+);
+
+CREATE TABLE stats (
+    id SERIAL PRIMARY KEY,
+    "name" citext NOT NULL
+);
+
+CREATE TABLE locations (
+    id BIGSERIAL PRIMARY KEY,
+    map_id BIGINT NOT NULL,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL
+);
+
+CREATE TABLE maps (
+    id SERIAL PRIMARY KEY,
+    "name" citext NOT NULL,
+    data text NOT NULL
+);
+
+CREATE TABLE map_tiles (
+    id BIGSERIAL PRIMARY KEY,
+    map_id INT NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL
+);
+
+CREATE TABLE players (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    location_id BIGINT NOT NULL,
+    "name" citext NOT NULL
+);
+
+CREATE TABLE player_stats (
+    id BIGSERIAL PRIMARY KEY,
+    player_id BIGINT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE player_growth_stats (
+    id BIGSERIAL PRIMARY KEY,
+    player_id BIGINT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE items (
+    id BIGSERIAL PRIMARY KEY,
+    player_id BIGINT NULL,
+    npc_id INT NULL,
+    location_id BIGINT NULL,
+    "name" citext NOT NULL
+);
+
+CREATE TABLE item_stats (
+    id BIGSERIAL PRIMARY KEY,
+    item_id BIGINT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE item_growth_stats (
+    id BIGSERIAL PRIMARY KEY,
+    item_id BIGINT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE npcs (
+    id SERIAL PRIMARY KEY,
+    location_id BIGINT NOT NULL,
+    "name" citext NOT NULL
+);
+
+CREATE TABLE npc_stats (
+    id BIGSERIAL PRIMARY KEY,
+    npc_id INT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE npc_growth_stats (
+    id BIGSERIAL PRIMARY KEY,
+    npc_id INT NOT NULL,
+    stat_id INT NOT NULL,
+    value BIGINT NOT NULL
+);
+
+CREATE TABLE objects (
+    id BIGSERIAL PRIMARY KEY,
+    location_id BIGINT NOT NULL,
+    "name" citext NOT NULL
+);
+
+CREATE TABLE settings (
+    "name" citext NOT NULL,
+    value text not null;
+);
+
+ALTER TABLE script_triggers ADD CONSTRAINT "script_triggers_scripts_id_fkey" FOREIGN KEY (script_id) REFERENCES scripts(id);
+ALTER TABLE locations ADD CONSTRAINT "locations_maps_id_fkey" FOREIGN KEY (map_id) REFERENCES maps(id);
+ALTER TABLE map_tiles ADD CONSTRAINT "map_tiles_maps_id_fkey" FOREIGN KEY (map_id) REFERENCES maps(id);
+ALTER TABLE players ADD CONSTRAINT "players_locations_id_fkey" FOREIGN KEY (location_id) REFERENCES locations(id);
+ALTER TABLE players ADD CONSTRAINT "players_stats_id_fkey" FOREIGN KEY (stats_id) REFERENCES stats(id);
+ALTER TABLE player_stats ADD CONSTRAINT "player_stats_players_id_fkey" FOREIGN KEY (player_id) REFERENCES players(id);
+ALTER TABLE player_stats ADD CONSTRAINT "player_stats_stats_id_fkey" FOREIGN KEY (stat_id) REFERENCES stats(id);
+ALTER TABLE items ADD CONSTRAINT "items_players_id_fkey" FOREIGN KEY (player_id) REFERENCES players(id);
+ALTER TABLE items ADD CONSTRAINT "items_npcs_id_fkey" FOREIGN KEY (npc_id) REFERENCES npcs(id);
+ALTER TABLE items ADD CONSTRAINT "items_locations_id_fkey" FOREIGN KEY (location_id) REFERENCES locations(id);
+ALTER TABLE item_stats ADD CONSTRAINT "item_stats_items_id_fkey" FOREIGN KEY (item_id) REFERENCES items(id);
+ALTER TABLE item_stats ADD CONSTRAINT "item_stats_stats_id_fkey" FOREIGN KEY (stat_id) REFERENCES stats(id);
+ALTER TABLE npcs ADD CONSTRAINT "npcs_location_id_fkey" FOREIGN KEY (location_id) REFERENCES locations(id);
+ALTER TABLE item_stats ADD CONSTRAINT "item_stats_items_id_fkey" FOREIGN KEY (item_id) REFERENCES items(id);
+ALTER TABLE item_stats ADD CONSTRAINT "item_stats_stats_id_fkey" FOREIGN KEY (stat_id) REFERENCES stats(id);
+ALTER TABLE objects ADD CONSTRAINT "objects_locations_id_fkey" FOREIGN KEY (location_id) REFERENCES locations(id);
---
+++ src/main.cpp
@@ -0,0 +1,207 @@
+/*
+    Realm of Aesir backend
+    Copyright (C) 2016  Michael de Lang
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <easylogging++.h>
+#include <json.hpp>
+#include <kafka_consumer.h>
+#include <kafka_producer.h>
+#include <admin_messages/quit_message.h>
+
+#include <signal.h>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <vector>
+#include <thread>
+#include <unordered_map>
+#include <atomic>
+#include <exceptions.h>
+#include <roa_di.h>
+#include <macros.h>
+#include "message_handlers/message_dispatcher.h"
+#include "repositories/user_repository.h"
+#include "database_transaction.h"
+#include "config.h"
+
+using namespace std;
+using namespace roa;
+using namespace pqxx;
+
+#ifdef EXPERIMENTAL_OPTIONAL
+using namespace experimental;
+#endif
+
+using json = nlohmann::json;
+
+
+INITIALIZE_EASYLOGGINGPP
+
+atomic<bool> quit{false};
+
+void on_sigint(int sig) {
+    quit = true;
+}
+
+void init_extras() noexcept {
+    ios::sync_with_stdio(false);
+    signal(SIGINT, on_sigint);
+}
+
+void init_logger(Config const config) noexcept {
+    el::Configurations defaultConf;
+    defaultConf.setGlobally(el::ConfigurationType::Format, "%datetime %level: %msg");
+    if(!config.debug_level.empty()) {
+        if(config.debug_level == "error") {
+            defaultConf.set(el::Level::Warning, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Info, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Debug, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Trace, el::ConfigurationType::Enabled, "false");
+        } else if(config.debug_level == "warning") {
+            defaultConf.set(el::Level::Info, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Debug, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Trace, el::ConfigurationType::Enabled, "false");
+        } else if(config.debug_level == "info") {
+            defaultConf.set(el::Level::Debug, el::ConfigurationType::Enabled, "false");
+            defaultConf.set(el::Level::Trace, el::ConfigurationType::Enabled, "false");
+        } else if(config.debug_level == "debug") {
+            defaultConf.set(el::Level::Trace, el::ConfigurationType::Enabled, "false");
+        }
+    }
+    el::Loggers::reconfigureAllLoggers(defaultConf);
+    LOG(INFO) << "debug level: " << config.debug_level;
+}
+
+
+
+Config parse_env_file() {
+    string env_contents;
+    ifstream env(".env");
+
+    if(!env) {
+        LOG(ERROR) << "[main] no .env file found. Please make one.";
+        exit(1);
+    }
+
+    env.seekg(0, ios::end);
+    env_contents.resize(env.tellg());
+    env.seekg(0, ios::beg);
+    env.read(&env_contents[0], env_contents.size());
+    env.close();
+
+    auto env_json = json::parse(env_contents);
+    Config config;
+
+    try {
+        config.broker_list = env_json["BROKER_LIST"];
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] BROKER_LIST missing in .env file.";
+        exit(1);
+    }
+
+    try {
+        config.group_id = env_json["GROUP_ID"];
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] GROUP_ID missing in .env file.";
+        exit(1);
+    }
+
+    try {
+        config.server_id = env_json["SERVER_ID"];
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] SERVER_ID missing in .env file.";
+        exit(1);
+    }
+
+    if(config.server_id == 0) {
+        LOG(ERROR) << "[main] SERVER_ID has to be greater than 0";
+        exit(1);
+    }
+
+    try {
+        config.connection_string = env_json["CONNECTION_STRING"];
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] CONNECTION_STRING missing in .env file.";
+        exit(1);
+    }
+
+    try {
+        config.debug_level = env_json["DEBUG_LEVEL"];
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] DEBUG_LEVEL missing in .env file.";
+        exit(1);
+    }
+
+    return config;
+}
+
+int main() {
+    Config config;
+    try {
+        config = parse_env_file();
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "[main] .env file is malformed json.";
+        exit(1);
+    }
+
+    init_logger(config);
+    init_extras();
+
+    database_pool db_pool;
+    db_pool.create_connections(config.connection_string);
+    auto common_injector = create_common_di_injector();
+    auto backend_injector = boost::di::make_injector(
+            boost::di::bind<idatabase_transaction>.to<database_transaction>(),
+            boost::di::bind<idatabase_connection>.to<idatabase_connection>(),
+            boost::di::bind<idatabase_pool>.to(db_pool),
+            boost::di::bind<iuser_repository>.to<user_repository>());
+
+    auto producer = common_injector.create<shared_ptr<ikafka_producer<false>>>();
+    auto backend_consumer = common_injector.create<shared_ptr<ikafka_consumer<false>>>();
+    auto user_repo = backend_injector.create<user_repository>();
+    backend_consumer->start(config.broker_list, config.group_id, std::vector<std::string>{"server-" + to_string(config.server_id), "world_messages", "broadcast"});
+    producer->start(config.broker_list);
+
+
+    try {
+        message_dispatcher<false> backend_server_msg_dispatcher;
+
+        LOG(INFO) << "[main] starting main thread";
+
+        while (!quit) {
+            try {
+                producer->poll(10);
+                auto msg = backend_consumer->try_get_message(10);
+                if (get<1>(msg)) {
+
+                    backend_server_msg_dispatcher.trigger_handler(msg);
+                }
+            } catch (serialization_exception &e) {
+                cout << "[main] received exception " << e.what() << endl;
+            }
+        }
+
+        LOG(INFO) << "[main] closing";
+
+        producer->close();
+        backend_consumer->close();
+    } catch (const runtime_error& e) {
+        LOG(ERROR) << "[main] error: " << typeid(e).name() << "-" << e.what();
+    }
+
+    return 0;
+}
