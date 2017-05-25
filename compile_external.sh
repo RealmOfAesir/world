cff2e9b6a2d13fcdc01317bbf4f05743e08b1e71
Michael de Lang <kingoipo@gmail.com>
2017-05-25 23:27:27 +0200

    Update dependencies and add compile script

---
+++ compile_external.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CPUS="$( grep -c ^processor /proc/cpuinfo )"
+cd $DIR/external/common_backend
+./compile_external.sh
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+make -j$CPUS
+cd $DIR
