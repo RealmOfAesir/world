d73d9c93a088f68ba55c9a07512958928fa2ed11
Michael de Lang <kingoipo@gmail.com>
2017-09-01 19:02:56 +0200

    Remove entityx from compile_external

--- compile_external.sh
+++ compile_external.sh
@@ -7,11 +7,6 @@ cd $DIR/external/common_backend
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .
 make -j$CPUS
 
-cd $DIR/external/entityx
-# Release for entityx is -O2 -g -DNDEBUG
-cmake -DCMAKE_BUILD_TYPE=Release -DENTITYX_DT_TYPE=uint32_t -DENTITYX_MAX_COMPONENTS=32
-make -j$CPUS
-
 cd $DIR/external/LuaJIT
 make -j$CPUS
 cd $DIR
